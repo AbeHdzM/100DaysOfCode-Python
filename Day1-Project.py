0bbb4e61746170ba323b51966ad76f4c2f7daaa8
Abraham Hernandez <abehdzm@gmail.com>
2022-09-29 19:21:37 -0500

    First Day Complete

---
+++ Day1-Project.py
@@ -0,0 +1,4 @@
+# This program will generate the name for a band combining the city you grew up and the name of a pet.
+city_name = input("What's the name of the city you grew up in?\n")
+pet_name = input("What's the name of your pet?\n")
+print("Your band name could be: " + city_name + " " + pet_name)
--- helloworld.py
+++
@@ -1,2 +0,0 @@
-print("Hello world, starting the 100 days of code challenge")
-print("My name is Abraham")
