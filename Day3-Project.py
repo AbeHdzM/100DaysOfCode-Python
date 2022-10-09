fa8019d0015f64f98e35d1d469ac18a4a804247b
Abraham Hernandez <abehdzm@gmail.com>
2022-10-09 17:43:13 -0500

    Day3-Third project completed

---
+++ Day3-Project.py
@@ -0,0 +1,38 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+firstDecision = input("You're at a cross road. Where do you want to got? Type 'left' or 'right'")
+if firstDecision == "left":
+    secondDecision = input("You've reached a river near a mountain, do you 'swim' across or 'wait' to see if a boat arrives?")
+    if secondDecision == "swim":
+        thirdDecision = input("You arrive at a house with 2 doors at the front, do you pick 'blue' or 'red'?")
+        if thirdDecision == "blue":
+            print("Congrats, you found the treasure!")
+        else:
+            print("There is a monster in the room, you die.\nGame Over.")
+    else:
+        print("No one came, you ran out of food and starved to death.\nGame Over.")
+else:
+    print("A gang of bandits assaults you, you fight and die.\nGame Over.")
